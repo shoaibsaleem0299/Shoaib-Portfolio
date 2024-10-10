6f64afdcab89df2f15e66faad2dc8080c63b7c79
shoaibsaleem0299 <shoaib0299@gmail.com>
2024-10-11 02:08:14 +0500

    resolve the error

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Shoaib Saleem",
-  profile: '/profile.JPG',
+  profile: '/profile.PNG',
   designation: "Software Developer",
   description: "My name is Shoaib Saleem. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web and mobile application development and love to make the web and mobile app more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
   email: 'shoaib0299@gmail.com',
